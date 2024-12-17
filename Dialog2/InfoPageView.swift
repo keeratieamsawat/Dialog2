56ab7c14bd8be0fc7c668c1339691f19d89ae81f
ChenFan213 <ginny.fan22@imperial.ac.uk>
2024-12-17 17:24:13 +0000

    December 17th, 17:24 PM

--- Dialog2/InfoPageView.swift
+++ Dialog2/InfoPageView.swift
@@ -190,6 +190,13 @@ struct CreateAccountView: View {
 }
 
 struct PersonalInfoView: View {
+// MARK: storing user inputs to the create account pages
+
+    @State private var gender: String = ""
+    @State private var birthDate: Date = Date()
+    @State private var country: String = ""
+    @State private var emergContact: String = ""
+    
     var body: some View {
         VStack(alignment: .leading, spacing: 20) { // use alignment in VStack to align everything to the left
             
@@ -202,7 +209,72 @@ struct PersonalInfoView: View {
             Text("Personal Information")
                 .font(.title)
                 .bold() // Make it bold
+            
+// MARK: text fields to intake user inputs for creating account
+            
+            // gender
+            Text("Gender:")
+                .font(.headline)
+            TextField("Your gender", text: $gender)
+                .padding(10)
+                .frame(height: 40)
+                .overlay(
+                    RoundedRectangle(cornerRadius: 8)
+                        .stroke(Color("Primary_Color"), lineWidth: 2))
+                .offset(y:-10)
+            
+            // birth date
+            Text("Birth Date:")
+                .font(.headline)
+            ZStack {
+                RoundedRectangle(cornerRadius: 8)
+                    .stroke(Color("Primary_Color"), lineWidth: 2)
+                
+                DatePicker(
+                    "Select your birthday",
+                    selection: $birthDate,
+                    displayedComponents: [.date])
+                .padding(10)
+            }
+            .frame(height:20)
+                .datePickerStyle(CompactDatePickerStyle())
+            
+            // country of residence
+            Text("Country of Residence:")
+                .font(.headline)
+                .offset(y:10)
+            TextField("Enter your country of residence", text: $country)
+                .padding(10)
+                .frame(height: 40)
+                .overlay(
+                    RoundedRectangle(cornerRadius: 8)
+                        .stroke(Color("Primary_Color"), lineWidth: 2))
+            
+            // emergency contact details
+            Text("Emergency Contact:")
+                .font(.headline)
+                .offset(y:10)
+            TextField("Enter the details of your emergency contact", text: $emergContact)
+                .padding(10)
+                .frame(height: 40)
+                .overlay(
+                    RoundedRectangle(cornerRadius: 8)
+                        .stroke(Color("Primary_Color"), lineWidth: 2))
+            
+            // button to direct to next page
+            NavigationLink(destination: PersonalInfoView()) {
+                Text("Final Step")
+                    .bold()
+                    .frame(maxWidth:.infinity,minHeight: 50)
+                    .foregroundColor(.white)
+                    .background(Color("Primary_Color"))
+                    .cornerRadius(10)
+                    .padding(.horizontal,40)
+            }
+            
+            Spacer() // VStack spacer to push contents up
         }
+        .padding(40) // VStack padding to make content not touch the edges
     }
 }
 
@@ -214,5 +286,5 @@ struct SignInView: View {
 }
 
 #Preview {
-    CreateAccountView()
+    PersonalInfoView()
 }
