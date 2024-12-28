1e50655407689f7db17c5ce356edaeb06b7efb6f
keeratieamsawat <150912473+keeratieamsawat@users.noreply.github.com>
2024-12-28 14:19:09 +0000

    Initial Commit

---
+++ Dialog/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Dialog
+//
+//  Created by primproud on 28/12/2567 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Dialog/DialogApp.swift
@@ -0,0 +1,17 @@
+//
+//  DialogApp.swift
+//  Dialog
+//
+//  Created by primproud on 28/12/2567 BE.
+//
+
+import SwiftUI
+
+@main
+struct DialogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
