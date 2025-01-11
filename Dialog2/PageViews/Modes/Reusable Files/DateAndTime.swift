f8f11be1d5a1be6689dfc00b726d82169f960b50
keeratieamsawat <150912473+keeratieamsawat@users.noreply.github.com>
2025-01-11 15:13:51 +0000

    Update DateAndTime.swift

---
+++ Dialog2/PageViews/Modes/Reusable Files/DateAndTime.swift
@@ -0,0 +1,22 @@
+import Foundation
+
+struct DateUtils {
+    /// Formats a `Date` into a string using the specified format and the Gregorian calendar.
+    static func formattedDate(from date: Date, format: String = "MMM d, yyyy") -> String {
+        let formatter = DateFormatter()
+        formatter.dateFormat = format
+        formatter.calendar = Calendar(identifier: .gregorian) // Use Gregorian calendar
+        formatter.timeZone = TimeZone(secondsFromGMT: 0) // Ensure UTC
+        return formatter.string(from: date)
+    }
+
+    /// Formats a `Date` into a string for time using the specified format and the Gregorian calendar.
+    static func formattedTime(from time: Date, format: String = "HH:mm") -> String {
+        let formatter = DateFormatter()
+        formatter.dateFormat = format
+        formatter.calendar = Calendar(identifier: .gregorian) // Use Gregorian calendar
+        formatter.timeZone = TimeZone(secondsFromGMT: 0) // Ensure UTC
+        return formatter.string(from: time)
+    }
+}
+
