bb0946f95fca5d0fd63b7870e789893939e28f28
Chen Fan <gf422@ic.ac.uk>
2025-01-13 23:36:30 +0000

    Reviewed comments on MainPage

--- Dialog2/PageViews/Login/SignIn/MainPage.swift
+++ Dialog2/PageViews/Login/SignIn/MainPage.swift
@@ -1,6 +1,13 @@
+// MARK: main page of the app, with JavaCakes (our team name) and DiaLog logos imported as Assets on Xcode and used in this UI page
+
+// in Assets, we uploaded the JavaCakes logo and DiaLog logo, and they can be called directly in UI pages to be added to the UI page
+// we have also added "Primary Color" in the Assets, which defines the main color of our app. it is the universal blue symbol colour for diabetes
+
 import SwiftUI
 
 struct MainPageView: View {
+    
+    // this boolean variable is for navigation later. if the user is first time user, navigate to create account; if the user exists already, navigate to login
     @State private var isFirstTimeUser: Bool = UserDefaults.standard.bool(forKey: "isFirstTimeUser") != false
     @State private var navigateToCreateAccount = false
     @State private var navigateToLogin = false
@@ -8,7 +15,7 @@ struct MainPageView: View {
     @StateObject var userData = UserRegistrationData()
 
     var body: some View {
-        NavigationStack {
+        NavigationStack { // for nagivation to the next UI page
             VStack(spacing: 40.0) {
                 // JavaCakes logo
                 Image("JavaCakes")
@@ -36,6 +43,8 @@ struct MainPageView: View {
                     .foregroundColor(.black)
                     .offset(y: -160)
                 
+// REFERENCE 1 - OpenAI. (2025). ChatGPT (v. 4). Retrieved from https://chat.openai.com
+                
                 // Navigation links for dynamic navigation
                 NavigationLink("", destination: CreateAccountView(userData:userData), isActive: $navigateToCreateAccount)
                 NavigationLink("", destination: LoginView(), isActive: $navigateToLogin)
@@ -49,6 +58,8 @@ struct MainPageView: View {
                         navigateToLogin = true
                     }
                 }
+/* end of reference 1 */
+                
                 .bold()
                 .frame(maxWidth: .infinity, minHeight: 50)
                 .foregroundColor(.white)
