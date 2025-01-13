def46264e5181f21b31cacf7f378946135e834b6
keeratieamsawat <150912473+keeratieamsawat@users.noreply.github.com>
2025-01-13 22:36:59 +0000

    Add comment and reference

--- Dialog2/PageViews/Modes/BloodSugarAlert/BloodSugarAlertModel.swift
+++ Dialog2/PageViews/Modes/BloodSugarAlert/BloodSugarAlertModel.swift
@@ -1,8 +1,18 @@
 import SwiftUI
 
+// A struct representing a blood sugar alert. It conforms to Identifiable and Codable protocols.
 struct BloodSugarAlert: Identifiable, Codable {
-    var id = UUID() // Unique identifier for the alert
+    
+    // Unique identifier for the alert. This allows the alert to be uniquely identified in lists or other UI components.
+    var id = UUID()
+    
+    // The title of the alert (e.g., "Warning").
     var title: String
+    
+    // The message of the alert (e.g., "Your blood sugar is out of range.").
     var message: String
+    
+    // The title for the dismiss button in the alert (e.g., "OK, I have understood").
     var dismissButtonTitle: String // Button text to dismiss the alert
 }
+
