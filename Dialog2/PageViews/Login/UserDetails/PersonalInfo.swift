2d0f1675026cbfeb28251295b31226a9c2db969a
Chen Fan <gf422@ic.ac.uk>
2025-01-13 23:39:42 +0000

    Update PersonalInfo.swift

--- Dialog2/PageViews/Login/UserDetails/PersonalInfo.swift
+++ Dialog2/PageViews/Login/UserDetails/PersonalInfo.swift
@@ -1,9 +1,10 @@
+// MARK: these pages take in new users' personal information, and propagate into following pages and eventually sent to backend
+
 import SwiftUI
 
 struct PersonalInfoView: View {
-
-// MARK: storing user inputs to the create account pages
-
+    
+    // calling the data model
     @ObservedObject var userData = UserRegistrationData()
     
     var body: some View {
@@ -24,12 +25,12 @@ struct PersonalInfoView: View {
                 Text("Gender:")
                     .font(.headline)
                 TextField("Your gender", text: $userData.gender)
-                    .padding(10)
-                    .frame(height: 40)
+                    .padding(10) // this padding creates some blank before the text, so that the text does not touch the edge of the frame
+                    .frame(height: 40) // standardise height of the frames
                     .overlay(
-                        RoundedRectangle(cornerRadius: 8)
+                        RoundedRectangle(cornerRadius: 8) // make the frame a rectangle with round corners
                             .stroke(Color("Primary_Color"), lineWidth: 2))
-                    .offset(y:-10)
+                    .offset(y:-10) // slight offset in y-axis for better vertical alignments
                 
                 // birth date
                 Text("Birth Date:")
@@ -37,15 +38,15 @@ struct PersonalInfoView: View {
                 ZStack {
                     RoundedRectangle(cornerRadius: 8)
                         .stroke(Color("Primary_Color"), lineWidth: 2)
-                    
+                    // using a date picker for birth date selection
                     DatePicker(
                         "Select your birthday",
                         selection: $userData.birthDate,
                         displayedComponents: [.date])
                     .padding(10)
                 }
-                .frame(height:20)
-                .datePickerStyle(CompactDatePickerStyle())
+                .frame(height:20) // as there is a date picker inside, the frame height need to be adjusted slightly different from the other frames
+                .datePickerStyle(CompactDatePickerStyle()) // date picker style
                 
                 // country of residence
                 Text("Country of Residence:")
@@ -70,7 +71,7 @@ struct PersonalInfoView: View {
                             .stroke(Color("Primary_Color"), lineWidth: 2))
                 
                 // for weight and height
-                // using HStack and VStack to align them next to each other
+                // using HStack and VStack togehter, to align them next to each other
                 HStack(spacing: 20) {
                     VStack(alignment: .leading, spacing: 5) {
                         Text("Weight:")
@@ -90,7 +91,7 @@ struct PersonalInfoView: View {
                         TextField("Enter height", text: $userData.height)
                             .padding(10)
                             .frame(height: 40)
-                            .overlay(
+                            .overlay( 
                                 RoundedRectangle(cornerRadius: 10)
                                     .stroke(Color("Primary_Color"),lineWidth: 2))
                     }
